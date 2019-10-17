c9db2d45aa3fba97f83fda5ffe768bf349e49980
Ziyang Ma <maziyang1008@gmail.com>
2019-10-16 22:47:18 -0400

    small modifies

--- Leetcode Exercise/626.sql
+++ Leetcode Exercise/626.sql
@@ -7,3 +7,14 @@ FROM seat,
 (SELECT COUNT(*) AS COUNTS
 FROM SEAT) AS SEAT_COUNTS
 ORDER BY id ASC;
+
+---equivalent to cartesian join,
+SELECT 
+(CASE WHEN MOD(id,2)!=0 AND counts !=id then id+1
+      WHEN MOD(id,2)!=0 AND counts =id THEN id
+      ELSE id-1 end) as id, student
+FROM seat
+CROSS JOIN
+(SELECT COUNT(*) AS COUNTS
+FROM SEAT) as seat_counts
+ORDER BY id ASC;
